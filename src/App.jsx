e74d5294f5e1163005cd08288c0d3bbcacb96ffc
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2022-03-30 15:47:16 -0400

    feat(app): claim membership nft

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import { useAddress, useMetamask } from "@thirdweb-dev/react";
+import { useAddress, useEditionDrop, useMetamask } from "@thirdweb-dev/react";
+import { useEffect, useState } from "react";
+import { DROP_ADDRESS } from "./constants";
 
 const App = () => {
   // Use the hooks thirdweb give us.
   const address = useAddress();
   const connectWithMetamask = useMetamask();
+  const editionDrop = useEditionDrop(DROP_ADDRESS);
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  const [isClaiming, setIsClaiming] = useState(false);
+
   console.log("👋 Address:", address);
+
+  useEffect(() => {
+    if (!address) return;
+
+    async function checkUserHasClamedNFT() {
+      try {
+        const balance = await editionDrop.balanceOf(address, 0);
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log("🌟 this user has a membership NFT!");
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.");
+        }
+      } catch (error) {
+        setHasClaimedNFT(false);
+        console.error("Failed to get balance", error);
+      }
+    }
+
+    checkUserHasClamedNFT();
+  }, [address, editionDrop]);
 
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
@@ -19,11 +47,39 @@ const App = () => {
     );
   }
 
+  const mintNft = async () => {
+    try {
+      setIsClaiming(true);
+      await editionDrop.claim("0", 1);
+      console.log(
+        `🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`
+      );
+      setHasClaimedNFT(true);
+    } catch (error) {
+      setHasClaimedNFT(false);
+      console.error("Failed to mint NFT", error);
+    } finally {
+      setIsClaiming(false);
+    }
+  };
+
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>🛩️DAO Member Page</h1>
+        <p>Congratulations on being a member</p>
+      </div>
+    );
+  }
+
   // This is the case where we have the user's address
   // which means they've connected their wallet to our site!
   return (
-    <div className="landing">
-      <h1>👀 wallet connected, now what!</h1>
+    <div className="mint-nft">
+      <h1>Mint your free 🛩️DAO Membership NFT</h1>
+      <button disabled={isClaiming} onClick={mintNft}>
+        {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
+      </button>
     </div>
   );
 };
