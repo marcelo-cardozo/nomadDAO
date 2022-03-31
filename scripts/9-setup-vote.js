c8392375c3844ad91d45f9792440cc07079d7229
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2022-03-30 23:07:05 -0400

    feat(contract): voting contract

---
+++ scripts/9-setup-vote.js
@@ -0,0 +1,43 @@
+import sdk from "./1-initialize-sdk.js";
+import { GOV_TOKEN_ADDRESS, VOTING_CONTRACT_ADDRESS } from "./constants.js";
+
+// This is our governance contract.
+const vote = sdk.getVote(VOTING_CONTRACT_ADDRESS);
+
+// This is our ERC-20 contract
+const token = sdk.getToken(GOV_TOKEN_ADDRESS);
+
+(async () => {
+  try {
+    // Give our treasury the power to mint additional token if needed.
+    await token.roles.grant("minter", vote.getAddress());
+
+    console.log(
+      "Successfully gave vote contract permissions to act on token contract"
+    );
+  } catch (error) {
+    console.error(
+      "failed to grant vote contract permissions on token contract",
+      error
+    );
+    process.exit(1);
+  }
+
+  try {
+    // Grab our wallet's token balance, remember -- we hold basically the entire supply right now!
+    const ownedTokenBalance = await token.balanceOf(process.env.WALLET_ADDRESS);
+
+    // Grab 90% of the supply that we hold.
+    const ownedAmount = ownedTokenBalance.displayValue;
+    const percent90 = Math.floor((Number(ownedAmount) / 100) * 90);
+
+    // Transfer 90% of the supply to our voting contract.
+    await token.transfer(vote.getAddress(), percent90);
+
+    console.log(
+      "✅ Successfully transferred " + percent90 + " tokens to vote contract"
+    );
+  } catch (err) {
+    console.error("failed to transfer tokens to vote contract", err);
+  }
+})();
